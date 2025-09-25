e72185d204816a05c08cd5bf3d99c5190dd0a4b3
tscott12331 <t.scott12331@gmail.com>
2025-09-25 14:49:48 -0700

    Add unit creation button functionality

--- Assets/Scripts/UnitFactoryController.cs
+++ Assets/Scripts/UnitFactoryController.cs
@@ -8,24 +8,29 @@ public class UnitFactoryController : MonoBehaviour
     [SerializeField]
     Transform spawnPositionTransform;
     [SerializeField]
+    Transform walkPositionTransform;
+    [SerializeField]
     LayerMask groundLayer;
 
     const float MAX_SAMPLE_DIST = 100.0f;
 
 
     void UIManager_onBasicUnitCreate() {
-        Debug.Log("Here");
         NavMeshHit navMeshHit;
-        if(NavMesh.SamplePosition(spawnPositionTransform.position, out navMeshHit, MAX_SAMPLE_DIST, groundLayer)) {
+        if(NavMesh.SamplePosition(spawnPositionTransform.position, out navMeshHit, MAX_SAMPLE_DIST, NavMesh.AllAreas)) {
             GameObject unit = Instantiate(basicUnitPrefab);
             unit.transform.position = navMeshHit.position;
             UnitManager.Instance.addUnit(unit);
+
+            if(NavMesh.SamplePosition(walkPositionTransform.position, out navMeshHit, MAX_SAMPLE_DIST, NavMesh.AllAreas))
+            {
+                unit.GetComponent<NavMeshAgent>().SetDestination(navMeshHit.position);
+            }
         }
 
     }
 
     void OnEnable() {
-        Debug.Log("adding listener");
         UIManager.onBasicUnitCreate += UIManager_onBasicUnitCreate;
     }
 
