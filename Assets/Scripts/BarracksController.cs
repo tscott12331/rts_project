49f2517f016adb6aba9adf3803c5d52925a787dd
tscott12331 <t.scott12331@gmail.com>
2025-10-03 15:03:43 -0700

    Rename UnitFactoryController class to BarracksController

--- Assets/Scripts/BarracksController.cs
+++ Assets/Scripts/BarracksController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class UnitFactory : Structure
+public class BarracksController : Structure
 {
     [SerializeField]
     GameObject basicUnitPrefab;
