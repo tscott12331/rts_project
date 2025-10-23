9dc1a18a62b3015f6ad080bde48e4525906a8822
Thomas Scott <thomas.scott.837@my.csun.edu>
2025-10-22 20:35:19 -0700

    WIP dynamic ui

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public delegate void OnBasicUnitCreate();
     public static event OnBasicUnitCreate onBasicUnitCreate;
 
+    public GameObject UnitPanel;
+    public GameObject UpgradePanel;
+
     public void OnBasicUnitButtonClick()
     {
         onBasicUnitCreate?.Invoke();
+    }
+
+    public void enableUnitPanel(Dictionary<int, GameObject> units) {
+        UnitPanel.SetActive(true);
+        var buttons = UnitPanel.GetComponents<Button>();
+        foreach(var unit in units) {
+
+        }
     }
 }
