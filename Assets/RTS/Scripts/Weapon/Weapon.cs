70b4d0a2d95b7e3581e253ee842a6dc16002b59d
tscott12331 <t.scott12331@gmail.com>
2025-12-14 20:52:45 -0800

    Add shooting sounds

--- Assets/RTS/Scripts/Weapon/Weapon.cs
+++ Assets/RTS/Scripts/Weapon/Weapon.cs
@@ -8,6 +8,8 @@ public class Weapon : MonoBehaviour
 
     // particle system to instantiate when firing
     public GameObject ShootParticleSystem;
+
+    public AudioSource ShootSound;
     
     // enable particle system at all the barrels in the weapon
     public void Shoot()
@@ -15,6 +17,7 @@ public class Weapon : MonoBehaviour
         foreach(var barrel in Barrels)
         {
             Instantiate(ShootParticleSystem, barrel);
+            if(ShootSound != null) ShootSound.PlayOneShot(ShootSound.clip);
         }
     }
 }
